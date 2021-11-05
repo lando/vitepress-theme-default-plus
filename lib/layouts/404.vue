fc39006e28857bf43208955a6136b11921cf01cd
John Ouellet <john@thinktandem.io>
2021-11-05 11:52:10 -0400

    Add in start of theme

---
+++ lib/layouts/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <ParentLayout></ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@vuepress/theme-default/lib/client/layouts/404.vue'
+
+export default {
+  components: {ParentLayout},
+};
+</script>
+
+<style lang="scss">
+@import '../styles/index.scss';
+@import '@vuepress/plugin-palette/palette';
+</style>
