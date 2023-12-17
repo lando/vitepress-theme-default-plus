bc0f2fc08898dcd7b91f4599fa62f120430bb4f7
Mike Pirog <mike@lando.dev>
2023-12-17 14:03:31 -0500

    RESCAFFOLD 3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
-## v1.0.0-alpha.1 - [January 1, 2024](https://github.com/lando/vitepress-theme-default-plus/releases/tag/v1.0.0-alpha.1)
+## v1.0.0-beta.1 - [January 1, 2024](https://github.com/lando/vitepress-theme-default-plus/releases/tag/v1.0.0-alpha.1)
+
+* Migrated [@lando/vuepress-theme-default-plus](https://github.com/lando/vuepress-theme-default-plus) to [VitePress](https://vitepress.dev/)
--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 
-import {defineConfigWithTheme} from 'vitepress';
 import merge from 'lodash/merge';
+import Debug from 'debug';
+
+import {defineConfigWithTheme} from 'vitepress';
 
 import {defineConfig as parentDefineConfig} from '@jcamp/vitepress-blog-theme/config';
 
-export function defineConfig(config = {}) {
+export function defineConfig(userConfig = {}) {
+  const debug = Debug('@lando/vpltheme'); // eslint-disable-line
+  const config = merge({}, parentDefineConfig(), userConfig);
+  debug('initial vitepress configuration %O', config);
   return defineConfigWithTheme(merge({}, parentDefineConfig(), config));
 }
