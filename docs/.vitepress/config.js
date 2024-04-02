3fcba1ef4bfed21782a5ff8997492aabcb902e69
Mike Pirog <mike@lando.dev>
2024-04-02 11:18:49 -0400

    improve sponsor.data to accept yaml or json from a url part 2

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -40,7 +40,6 @@ export default defineConfig({
     },
   },
   themeConfig: {
-    landoDocs: 3,
     autometa: {
       canonicalUrl: 'https://vitepress-theme-default-plus.lando.dev/',
       image: 'https://vitepress-theme-default-plus.lando.dev/images/hero.png',
