2d813cf49a1bbee88cc8b0d0638991f7b6d2787f
Mike Pirog <mike@lando.dev>
2024-09-27 17:47:59 -0400

    nm i like the v- more

--- client/use-tags.js
+++ client/use-tags.js
@@ -9,16 +9,13 @@ export default function useTags() {
 
   // generate links we can pass into VPLVersionLink
   const links = tags.versions
-    .map(version => {
-      const {semantic} = tags.extended.find(v => v.ref === version);
-      return {
-        text: version,
-        href: `/${base}/${semantic}/`.replace(/\/{2,}/g, '/'),
-        prerelease: /^v?\d+\.\d+\.\d+-\S+$/.test(version),
-        stable: tags?.aliases?.stable === version,
-        edge: tags?.aliases?.edge === version,
-      };
-    });
+    .map(version => ({
+      text: version,
+      href: `/${base}/${version}/`.replace(/\/{2,}/g, '/'),
+      prerelease: /^v?\d+\.\d+\.\d+-\S+$/.test(version),
+      stable: tags?.aliases?.stable === version,
+      edge: tags?.aliases?.edge === version,
+    }));
 
   return {...tags, links};
 }
