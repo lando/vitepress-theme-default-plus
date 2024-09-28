52cb6a343d920b86dff2e852295f5329bf005606
Mike Pirog <mike@lando.dev>
2024-09-27 23:18:40 -0400

    attempt mvb on netlify

--- utils/parse-stdout.js
+++ utils/parse-stdout.js
@@ -1,6 +1,6 @@
 import {execSync} from 'node:child_process';
 
-export default function(cmd, options) {
+export default function(cmd, options = {}) {
   const stdout = execSync(cmd, {maxBuffer: 1024 * 1024 * 10, encoding: 'utf-8', ...options});
   return options.trim === true ? stdout.trim() : stdout;
 };
