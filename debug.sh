e64d6cde87c7cbce2420d86089edc0d27398c3fc
Mike Pirog <mike@lando.dev>
2024-09-29 14:19:16 -0400

    attempt mvb on netlify part 31

--- debug.sh
+++ debug.sh
@@ -8,11 +8,14 @@ env
 
 cat .git/config
 
+git fetch --unshallow || true
+
 git status
 git --no-pager branch
 
-git fetch origin --tags --no-filter
+git checkout multi-version-build
 
+git fetch origin --tags --no-filter
 git rev-parse --abbrev-ref --symbolic-full-name @{u}
 
-exit 1
+npx mvb --debug
