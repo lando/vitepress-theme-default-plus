b2e5c2b612e5bfe2cc5910d9848379451ceeba80
Mike Pirog <mike@lando.dev>
2024-09-29 13:35:32 -0400

    attempt mvb on netlify part 24

--- debug.sh
+++ debug.sh
@@ -6,27 +6,8 @@ pwd
 
 env
 
-git rev-parse --is-shallow-repository
-git rev-list --objects --all --missing=print
 cat .git/config
 
 git fetch origin --tags --no-filter
-
-git checkout v1.0.2
-git status
-git reset v1.0.2 --hard
-git status
-git reset HEAD --hard
-
-cd /tmp
-ls -lsa ./
-
-git clone /opt/build/repo repo
-cd repo
-
-git checkout v1.0.2
-git status
-git reset v1.0.2 --hard
-git status
 
 exit 1
