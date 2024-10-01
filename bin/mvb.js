9470f440fc2ee1b923e37a5b867a9323e30c7514
Mike Pirog <mike@lando.dev>
2024-10-01 11:39:33 -0400

    debug 24

--- bin/mvb.js
+++ bin/mvb.js
@@ -125,6 +125,9 @@ const shallow = getStdOut('git rev-parse --is-shallow-repository', {trim: true})
 if (shallow) updateArgs.push('--unshallow');
 // update all refs
 await oexec('git', updateArgs);
+await oexec('git', ['--no-pager', 'log', '-1']);
+await oexec('git', ['checkout', 'remotes/pull/51/merge']);
+
 
 // build clone args
 const cloneArgs = ['clone', '--origin', 'origin', '--no-single-branch'];
@@ -137,7 +140,8 @@ await exec('git', cloneArgs);
 await exec('ls', ['-lsa']);
 await exec('git', ['--no-pager', 'tag']);
 await exec('git', ['--no-pager', 'branch', '--all']);
-await exec('git', ['checkout', 'remotes/pull/51/merge']);
+await exec('git', ['status']);
+await exec('git', ['--no-pager', 'log', '-1']);
 
 // get extended version information
 const {extended} = await getTags(options.tmpDir, options);
