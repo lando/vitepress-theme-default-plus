66511ddd4dd7a68f0f92ee2cbf10885dd6bda1ab
Mike Pirog <mike@lando.dev>
2024-10-01 10:19:19 -0400

    debug 12

--- bin/mvb.js
+++ bin/mvb.js
@@ -144,8 +144,8 @@ await exec('git', ['--no-pager', 'tag', '--list', options.match]);
 await exec('git', ['--no-pager', 'branch']);
 await exec('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
 // await exec('git', ['describe', '--debug', '--tags', '--always', '--abbrev=1', `--match'"${options.match}"`, getBranch()]);
-await exec('git', ['describe', '--debug', '--tags', '--always', '--abbrev=1', 'origin/mvb-cache']);
-await exec('git', ['describe', '--debug', '--tags', '--always', '--abbrev=1']);
+await oexec('git', ['describe', '--debug', '--tags', '--always', '--abbrev=1', 'origin/mvb-cache']);
+await oexec('git', ['describe', '--debug', '--tags', '--always', '--abbrev=1']);
 
 // get extended version information
 const {extended} = await getTags(options.tmpDir, options);
