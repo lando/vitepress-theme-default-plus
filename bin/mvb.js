1619ae57b7cb3c7fbc04bb4ecce0a2c925fb9f4d
Mike Pirog <mike@lando.dev>
2024-10-01 12:09:03 -0400

    remove debugging from test and build part 2

--- bin/mvb.js
+++ bin/mvb.js
@@ -124,8 +124,14 @@ if (getStdOut('git rev-parse --is-shallow-repository', {trim: true}) === 'true')
 // update all refs
 await oexec('git', updateArgs);
 
+await oexec('git', ['status']);
+console.log(getStdOut('git rev-parse --abbrev-ref HEAD', {trim: true}));
+await oexec('git', updateArgs);
+
 // if we are in detached head state then checkout best branch
 if (getStdOut('git rev-parse --abbrev-ref HEAD', {trim: true}) === 'HEAD') await oexec('git', ['checkout', getBranch()]);
+
+process.exit(1)
 
 // build clone args
 const cloneArgs = ['clone', '--origin', 'origin', '--no-single-branch'];
