82042aa8c86afff729e2f78c32c386eee12d72d1
Mike Pirog <mike@lando.dev>
2024-10-02 14:28:18 -0400

    update default config sets part 4

--- config/landov4.js
+++ config/landov4.js
@@ -1,17 +1,19 @@
 import {default as isDevRelease} from '@lando/vitepress-theme-default-plus/is-dev-release';
 
 export default function({landoPlugin, themeConfig, version}) {
+  // reset the base if its undefined
+  if (!base) base = landoPlugin ? `/plugins/${landoPlugin}/` : '/';
+
   // allow version to imported from ENV which is nice for one-off dev builds
   version = process?.env?.LANDO_MVB_VERSION ? process.env.LANDO_MVB_VERSION : `v${version}`;
 
   // construct the rest
   const mvbase = themeConfig?.multiVersionBuild?.base ?? '/v/';
-  const baseUrl = landoPlugin ? `https://docs.lando.dev/${landoPlugin}` : 'https://docs.lando.dev';
-  const repo = landoPlugin ? `https://github.com/lando/${landoPlugin}` : 'https://github.com/lando';
-  const base = landoPlugin ? `/plugins/${landoPlugin}/` : '/';
+  const vbase = `${base.slice(0, -1)}${mvbase}`;
 
   const text = ['core', 'cli'].includes(landoPlugin) ? version : `${landoPlugin}@${version}`;
-  const vbase = ['core', 'cli'].includes(landoPlugin) ? `/${landoPlugin}${mvbase}` : `${base.slice(0, -1)}${mvbase}`;
+  const baseUrl = landoPlugin ? `https://docs.lando.dev/${landoPlugin}` : 'https://docs.lando.dev';
+  const repo = landoPlugin ? `https://github.com/lando/${landoPlugin}` : 'https://github.com/lando';
 
   const sidebarEnder = landoPlugin && version ? {
     text,
@@ -22,7 +24,7 @@ export default function({landoPlugin, themeConfig, version}) {
         items: [
           {rel: 'mvb', text: 'stable', target: '_blank', link: `${vbase}stable/`},
           {rel: 'mvb', text: 'edge', target: '_blank', link: `${vbase}edge/`},
-          {text: '<strong>see all versions</strong>', link: `${vbase}`},
+          {text: '<strong>see all versions</strong>', link: mvbase},
         ],
       },
       {text: 'Other Releases', link: `${repo}/releases`},
