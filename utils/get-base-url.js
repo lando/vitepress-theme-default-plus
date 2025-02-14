f69e7d46ade62f058199b51d36349cf8a156ea2c
Mike Pirog <mike@lando.dev>
2025-02-14 13:19:11 -0500

    fix bug causing mvb and vitepress build to fail when landoPlugin is set but VPL_BASE_URL or NETLIFYT are not

--- utils/get-base-url.js
+++ utils/get-base-url.js
@@ -4,12 +4,13 @@ export default function(landoPlugin) {
   // if VPL_BASE_URL is set then use that
   if (process.env?.VPL_BASE_URL) return process.env?.VPL_BASE_URL;
 
-  // if lando plugin and netlify prod then set to docs.lando.dev
-  if (landoPlugin && process.env?.NETLIFY && process.env.CONTEXT === 'production') return 'https://docs.lando.dev';
-
   // otherwise we can try other stuff if we are on something like netlify
+  if (process.env?.NETLIFY && process.env.CONTEXT === 'production' && landoPlugin) return 'https://docs.lando.dev';
   if (process.env?.NETLIFY && process.env.CONTEXT === 'production') return process.env.URL;
   if (process.env?.NETLIFY && process.env.CONTEXT !== 'production') return process.env.DEPLOY_PRIME_URL;
+
+  // if we get here and its a lando plugin we can safely assume https://docs.lando.dev, this is mostly for github actions testing
+  if (landoPlugin) return 'https://docs.lando.dev';
 
   // return nothing
   return undefined;
