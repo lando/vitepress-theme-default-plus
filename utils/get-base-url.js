4d308abe919004ea32ba2a441e6378a43de662ea
Mike Pirog <mike@lando.dev>
2025-02-14 11:52:31 -0500

    update landoplugin get base changes to use full https protocol absolute url

--- utils/get-base-url.js
+++ utils/get-base-url.js
@@ -5,7 +5,7 @@ export default function(landoPlugin) {
   if (process.env?.VPL_BASE_URL) return process.env?.VPL_BASE_URL;
 
   // if lando plugin and netlify prod then set to docs.lando.dev
-  if (landoPlugin && process.env?.NETLIFY && process.env.CONTEXT === 'production') return 'docs.lando.dev';
+  if (landoPlugin && process.env?.NETLIFY && process.env.CONTEXT === 'production') return 'https://docs.lando.dev';
 
   // otherwise we can try other stuff if we are on something like netlify
   if (process.env?.NETLIFY && process.env.CONTEXT === 'production') return process.env.URL;
